6cd06303d7ea9c269fa1877d954642ca0b7a0f5b
DenisovJr <denisov2gis@gmail.com>
2016-05-27 14:50:13 +0600

    Add Nlog

---
+++ OmnideskExtractor/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NLog;
+
+namespace Extractor
+{
+    class NLogger
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void InfoLog(string message)
+        {
+            try
+            {
+                Logger.Info(message);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Fail to write InfoLog: " + e);
+            }
+        }
+
+        public static void ErrorLog(string message)
+        {
+            try
+            {
+                Logger.Error(message);
+            }
+            catch
+            {
+                //throw;
+            }
+        }
+    }
+}
